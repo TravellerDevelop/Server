3241bcb031b315ea8d7680d14f16f267a76f4908
PietroBossolasco <p.bossolasco.1715@vallauri.edu>
2023-11-21 23:54:35 +0100

    risoluzione bug

--- func/post.ts
+++ func/post.ts
@@ -3,7 +3,7 @@ import NodeCache from "node-cache";
 import { DB_NAME } from "../server";
 import fs from "fs";
 
-export function createPost(req, res, cache) {
+export function createPost(req, res, cache, next) {
     let collection = req["connessione"].db(DB_NAME).collection("posts");
     let param = req.body.param;
     param.dateTime = new Date();
@@ -14,15 +14,17 @@ export function createPost(req, res, cache) {
             res.status(200).send(data);
         }
         cache.del("travel-post=" + param.travel);
+        next();
     });
 }
 
-export function takePosts(req, res, cache) {
+export function takePosts(req, res, cache, next) {
     let travel = req.query.travel;
     let cachedData = cache.get("travel-post=" + travel);
     if (cachedData) {
         res.send(cachedData).status(200)
         cache.set("travel-post=" + travel, cachedData, 600);
+        next();
     }
     else {
         let collection = req["connessione"].db(DB_NAME).collection("posts");
@@ -34,11 +36,12 @@ export function takePosts(req, res, cache) {
                 cache.set("travel-post=" + travel, data, 600);
                 res.status(200).send(data);
             }
+            next();
         });
     }
 }
 
-export function updateVote(req, res) {
+export function updateVote(req, res, next) {
     let id = req.body.id;
     let vote = req.body.vote;
 
@@ -49,16 +52,18 @@ export function updateVote(req, res) {
         } else {
             res.status(200).send(data);
         }
+        next();
     });
 }
 
-export function takeLastsPostByUsername(req, res, cache) {
+export function takeLastsPostByUsername(req, res, cache, next) {
     let username = req.query.username;
     let userid = req.query.userid;
     req["connessione"].db(DB_NAME).collection("travels").find({ "participants.userid": userid, "participants.username": username }).toArray(function (err: any, data: any) {
         if (err) {
             console.log("Errore esecuzione query");
             res.status(500).send("Errore esecuzione query");
+            next();
         }
         else {
             let ausData = [];
@@ -81,12 +86,13 @@ export function takeLastsPostByUsername(req, res, cache) {
 
                     res.status(200).send([data, otherData]);
                 }
+                next();
             });
         }
     });
 }
 
-export function updatePayment(req, res) {
+export function updatePayment(req, res, next) {
     let id = req.body.id;
     let destinator = req.body.destinator;
     req["connessione"].db(DB_NAME).collection("posts").updateOne({ _id: new ObjectId(id) }, { $set: { destinator: destinator } }, function (err: any, data: any) {
@@ -95,10 +101,11 @@ export function updatePayment(req, res) {
         } else {
             res.status(200).send(data);
         }
+        next();
     });
 }
 
-export function updatePinPost(req, res) {
+export function updatePinPost(req, res, next) {
     let id = req.body.param._id;
     let pinned = req.body.param.pinned;
 
@@ -110,14 +117,16 @@ export function updatePinPost(req, res) {
             console.log("data: ", data);
             res.status(200).send(data);
         }
+        next();
     });
 }
 
-export function deletePost(req, res) {
+export function deletePost(req, res, next) {
     let id = req.body.id;
     req["connessione"].db(DB_NAME).collection("posts").findOne({ _id: new ObjectId(id) }, function (err: any, data: any) {
         if (err) {
             res.status(500).send("Errore esecuzione query");
+            next();
         } else {
             if (typeof data.type != "undefined") {
                 if (data.type == "images") {
@@ -137,8 +146,9 @@ export function deletePost(req, res) {
                 if (err) {
                     res.status(500).send("Errore esecuzione query");
                 } else {
-                    res.status(200).send(data);
+                    res.status(200).send(data)
                 }
+                next();
             });
         }
     });
--- func/travels.ts
+++ func/travels.ts
@@ -3,7 +3,7 @@ import NodeCache from "node-cache";
 import { DB_NAME } from "../server";
 import fs from "fs";
 
-export function createTravel(req: any, res: any, cache: any) {
+export function createTravel(req: any, res: any, cache: any, next) {
     let collection = req["connessione"].db(DB_NAME).collection("travels");
     let param: any = req.body;
     param.creation_date = new Date(param.creation_date);
@@ -14,10 +14,11 @@ export function createTravel(req: any, res: any, cache: any) {
         } else {
             res.status(200).send(data);
         }
+        next();
     });
 }
 
-export function joinTravel(req: any, res: any, cache: NodeCache) {
+export function joinTravel(req: any, res: any, cache: NodeCache, next) {
     let collection = req["connessione"].db(DB_NAME).collection("travels");
     let error = false;
     collection.find({ code: req.body.code })
@@ -25,6 +26,7 @@ export function joinTravel(req: any, res: any, cache: NodeCache) {
             if (err) {
                 res.status(500).send("Errore esecuzione query");
                 error = true;
+                next();
             } else {
                 if (data.length == 1) {
                     if (data[0].creator == req.body.username) {
@@ -54,6 +56,7 @@ export function joinTravel(req: any, res: any, cache: NodeCache) {
                                         } else {
                                             res.status(200).send(data);
                                         }
+                                        next();
                                     });
                                 }
                             }
@@ -62,18 +65,20 @@ export function joinTravel(req: any, res: any, cache: NodeCache) {
                 } else {
                     res.status(201).send("Codice viaggio non valido");
                     error = true;
+                    next();
                 }
             }
         });
 }
 
-export function takeJoinedTravels(req: any, res: any, cache: any) {
+export function takeJoinedTravels(req: any, res: any, cache: any, next) {
     let username = req.query.username;
     let cachedData = cache.get("joined-usn=" + username);
     let userid = req.query.userid
     if (cachedData) {
         cache.set("joined-id=" + userid, cachedData, 100);
         res.send(cachedData).status(200);
+        next();
     } else {
         req["connessione"].db(DB_NAME).collection("travels").find({ "participants.userid": userid, "participants.username": username, closed: false }).sort({ creation_date: -1 }).toArray(function (err: any, data: any) {
             if (err) {
@@ -83,21 +88,24 @@ export function takeJoinedTravels(req: any, res: any, cache: any) {
                 cache.set("joined-id=" + userid, data, 100);
                 res.status(200).send(data);
             }
+            next();
         });
     }
 }
 
-export function takeTravelsParticipants(req: any, res: any, cache: any) {
+export function takeTravelsParticipants(req: any, res: any, cache: any, next) {
     let travel = req.query.travel;
     let cachedData = cache.get("takeParticipants=" + travel);
     let collection = req["connessione"].db(DB_NAME).collection("travels");
     if (cachedData) {
         cache.set("takeParticipants=" + travel, cachedData, 600);
         res.send(cachedData).status(200);
+        next();
     } else {
         collection.find({ code: travel }).toArray(function (err: any, data: any) {
             if (err) {
                 res.status(500).send("Errore esecuzione query");
+                next();
             }
             else {
                 let participants = [];
@@ -115,19 +123,21 @@ export function takeTravelsParticipants(req: any, res: any, cache: any) {
                         cache.set("takeParticipants=" + travel, data, 600);
                         res.status(200).send(data);
                     }
+                    next();
                 });
             }
         });
     }
 }
 
-export function takeTravelByCreator(req: any, res: any, cache: any) {
+export function takeTravelByCreator(req: any, res: any, cache: any, next) {
     let username = req.query.username;
     let cachedData = cache.get("takeByCreator=" + username);
     let collection = req["connessione"].db(DB_NAME).collection("travels");
     if (cachedData) {
         cache.set("takeByCreator=" + username, cachedData, 600);
         res.send(cachedData).status(200);
+        next();
     } else {
         collection.find({ "participants": { "$elemMatch": { "username": username, "creator": true } } }, { "participants.$": 1 }).sort({ creation_date: -1 }).toArray(function (err: any, data: any) {
             if (err) {
@@ -137,11 +147,12 @@ export function takeTravelByCreator(req: any, res: any, cache: any) {
                 cache.set("takeByCreator=" + username, data, 600);
                 res.status(200).send(data);
             }
+            next();
         });
     }
 }
 
-export function updateTravel(req: any, res: any) {
+export function updateTravel(req: any, res: any, next) {
     let param = req.body.param;
     let id = req.body.id;
 
@@ -152,36 +163,41 @@ export function updateTravel(req: any, res: any) {
         else {
             res.status(200).send(data);
         }
+        next();
     });
 }
 
-export function closeTravel(req: any, res: any) {
+export function closeTravel(req: any, res: any, next) {
     let id = req.body.id;
     req["connessione"].db(DB_NAME).collection("travels").updateOne({ _id: new ObjectId(id) }, { $set: { closed: true } }, function (err: any, data: any) {
         if (err) {
             res.status(500).send("Errore esecuzione query 1");
         }
         else { res.status(200).send(data); }
+        next();
     });
 }
 
-export function deleteTravel(req: any, res: any) {
+export function deleteTravel(req: any, res: any, next) {
     let id = req.body.id;
     req["connessione"].db(DB_NAME).collection("travels").find({ _id: new ObjectId(id) }).toArray(function (err: any, data: any) {
         if (err) {
             res.status(500).send("Errore esecuzione query 1");
+            next();
         }
         else {
             if (data[0].image) {
                 fs.unlink("./static/userImage/" + data[0].image, (err: any) => {
                     if (err) {
                         console.log("Errore eliminazione immagine");
+                        next();
                     }
                 });
             }
             req["connessione"].db(DB_NAME).collection("travels").deleteOne({ _id: new ObjectId(id) }, function (err: any, data: any) {
                 if (err) {
                     res.status(500).send("Errore esecuzione query 1");
+                    next();
                 }
                 else {
                     req["connessione"].db(DB_NAME).collection("posts").deleteMany({ travel: id }, function (err: any, data: any) {
@@ -191,6 +207,7 @@ export function deleteTravel(req: any, res: any) {
                         else {
                             res.status(200).send(data);
                         }
+                        next();
                     })
                 }
             });
@@ -198,19 +215,21 @@ export function deleteTravel(req: any, res: any) {
     });
 }
 
-export function leaveTravel(req: any, res: any, cache: any) {
+export function leaveTravel(req: any, res: any, cache: any, next) {
     let travel = req.body.travel;
     let userid = req.body.userid;
 
     req["connessione"].db(DB_NAME).collection("travels").findOne({ _id: new ObjectId(travel) }, function (err: any, data: any) {
         if (err) {
             res.status(500).send("Errore esecuzione query 1");
+            next();
         }
         else {
             if (data.participants.length == 1) {
                 req["connessione"].db(DB_NAME).collection("travels").deleteOne({ _id: new ObjectId(travel) }, function (err: any, data: any) {
                     if (err) {
                         res.status(500).send("Errore esecuzione query 1");
+                        next();
                     }
                     else {
                         req["connessione"].db(DB_NAME).collection("posts").deleteMany({ travel: travel }, function (err: any, data: any) {
@@ -220,6 +239,7 @@ export function leaveTravel(req: any, res: any, cache: any) {
                             else {
                                 res.status(200).send(data);
                             }
+                            next();
                         })
                     }
                 });
@@ -233,13 +253,14 @@ export function leaveTravel(req: any, res: any, cache: any) {
                     else {
                         res.status(200).send(data);
                     }
+                    next();
                 });
             }
         }
     });
 }
 
-export function uploadImage(req: any, res: any) {
+export function uploadImage(req: any, res: any, next) {
     let img = req.body.img;
     let imgName = req.body.imgName;
     let newName = Math.random().toString(36).substring(2, 20) + Math.random().toString(36).substring(2, 20);
@@ -256,5 +277,6 @@ export function uploadImage(req: any, res: any) {
             res.status(200);
             res.send(newName + "." + ext);
         }
+        next();
     });
 }
